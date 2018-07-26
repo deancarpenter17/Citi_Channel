25fda6e55a1c4ec6377e80b86653f1c0594ac099
JayPhippss <PhippsJaylin@gmail.com>
2018-07-26 15:31:24 -0500

    Filter Screen and reply label fixed

--- InternProject/Controllers/HomeViewController.swift
+++ InternProject/Controllers/HomeViewController.swift
@@ -80,5 +80,6 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         self.navigationItem.title = "Home"
         tableView.tableFooterView = UIView()
     }
-
+    
+    
 }
